43625fc0b3c5a18e2bee59ae3243762d1992234e
Osiris Prime <132836318+OsirisPrime@users.noreply.github.com>
2025-02-28 15:00:44 +0100

    Update Step3.vhd

--- Lab4/Step3.vhd
+++ Lab4/Step3.vhd
@@ -2,29 +2,29 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
+-- Converts seconds and minutes to BCD
 entity BCD_Converter is
     Port ( 
-          seconds : in integer range 0 to 59;          
-          minutes : in integer range 0 to 59;
-          bcd : out std_logic_vector(15 downto 0)
+          ss : in integer range 0 to 59;            -- Seconds input          
+          mm : in integer range 0 to 59;            -- Minutes input
+          bcd : out std_logic_vector(15 downto 0)   -- BCD output
           );
-end bcd_converter;
+end;
 
 architecture Arch_BCD of BCD_Converter is
 begin
-    process (minutes, seconds)
+    process(ss, mm) is
     begin
-        -- Convert minutes to BCD
-        bcd(15 downto 12) <= std_logic_vector(to_unsigned(minutes / 10, 4));    -- tens place of minutes
-        bcd(11 downto 8)  <= std_logic_vector(to_unsigned(minutes mod 10, 4));  -- Ones place of minutes
-        
-        -- Convert seconds to BCD
-        bcd(7 downto 4)   <= std_logic_vector(to_unsigned(seconds / 10, 4));    -- tens place of seconds
-        bcd(3 downto 0)   <= std_logic_vector(to_unsigned(seconds mod 10, 4));  -- Ones place of seconds
+        bcd(3 downto 0)   <= std_logic_vector(to_unsigned(ss mod 10, 4));  -- Seconds Ones digit   
+        bcd(7 downto 4)   <= std_logic_vector(to_unsigned(ss/10, 4));      -- Seconds Tens digit
+        bcd(11 downto 8)  <= std_logic_vector(to_unsigned(mm mod 10, 4));  -- Minutes Ones digit       
+        bcd(15 downto 12) <= std_logic_vector(to_unsigned(mm/10, 4));      -- Minutes Tens digit
     end process;
 end;
 
+
 -- //////////////////////////////////////////////// tb //////////////////////////////////////////////// --
+
 
 library ieee;
 use ieee.std_logic_1164.all;
@@ -34,36 +34,36 @@ entity tb is end;
 architecture ARCH_tb of tb is
     component BCD_Converter is
         Port ( 
-              seconds : in integer range 0 to 59;        
-              minutes : in integer range 0 to 59;
+              ss : in integer range 0 to 59;        
+              mm : in integer range 0 to 59;
               bcd : out std_logic_vector(15 downto 0)
               );
     end component;
-    signal seconds, minutes : integer range 0 to 59;
+    signal ss, mm : integer range 0 to 59;
     signal bcd : std_logic_vector(15 downto 0);
 begin
-    BCD_inst : BCD_Converter port map(seconds => seconds, minutes => minutes, bcd => bcd);
+    BCD_inst : BCD_Converter port map(ss => ss, mm => mm, bcd => bcd);
     
     process is
     begin
-        seconds <= 0;
-        minutes <= 0;
+        ss <= 0;
+        mm <= 0;
         wait for 5 ns;
         
-        seconds <= 30;
-        minutes <= 11;
+        ss <= 45;
+        mm <= 23;
         wait for 5 ns;
         
-        seconds <= 55;
-        minutes <= 2;
+        ss <= 55;
+        mm <= 2;
         wait for 5 ns;
         
-        seconds <= 1;
-        minutes <= 37;
+        ss <= 1;
+        mm <= 37;
         wait for 5 ns;
         
-        seconds <= 59;
-        minutes <= 59;
+        ss <= 59;
+        mm <= 59;
         wait for 5 ns;
     end process;
 end;
